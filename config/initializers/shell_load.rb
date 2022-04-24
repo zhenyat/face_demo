080d7cc537ad337cccee31df6f1f4b6e9957942f
Zhenya Telyukov <telyukov@gmail.com>
2022-04-24 19:44:06 +0300

    shell

---
+++ config/initializers/shell_load.rb
@@ -0,0 +1,29 @@
+################################################################################
+# shell_load.rb:
+#   (1) Loads default config parameters from YAML file at Initialization Phase
+#   (2) Updates parameters with values set for the Application Run
+#
+#   Source: http://railscasts.com/episodes/85-yaml-configuration-file
+#
+#   11.10.2016  ZT Inherited from 95km
+#   24.04.2022  Adopted
+################################################################################
+
+# Gets default App configuration parameters
+filename = 'config/shell/default.yml'
+if File.exists? "#{Rails.root}/#{filename}"
+  SHELL_CONFIG = YAML.load_file("#{Rails.root}/#{filename}")[Rails.env]
+
+  # Updates default configuration parameters for the current Application Run
+  filename = 'config/shell/run.yml'
+  if File.exists? "#{Rails.root}/#{filename}"
+    run_config = YAML.load_file("#{Rails.root}/#{filename}")
+    SHELL_CONFIG.deep_merge! run_config if run_config.present?  # File contains values
+  end
+end
+
+# Sets Constants and Parameters for the App run
+filename = 'config/shell/settings.rb'
+if File.exists? "#{Rails.root}/#{filename}"
+  eval File.read("#{Rails.root}/#{filename}")
+end
---
+++ config/shell/settings.rb
@@ -0,0 +1,34 @@
+################################################################################
+#   settings.rb:  Sets Constants and other Parameters for the run
+#                 Called by: shell_load.rb
+#   Relative path MUST BE applied to public/images, otherwise - sprockets error
+#
+#   11.10.2016  Inherited from 95km
+#   09.11.2016  MULTILINGUAL added
+#   30.08.2017  'access_restricted' key is added
+#   24.04.2022  Adopted
+################################################################################
+
+##### Debugging   #####
+SHELL_DEBUG = SHELL_CONFIG['debug']['status']
+
+if SHELL_CONFIG['debug']['path'].nil? || SHELL_CONFIG['debug']['path'].empty?
+  SHELL_LOG_FILE = nil
+else
+  SHELL_LOG_FILE = "#{Rails.root}/#{SHELL_CONFIG['debug']['path']}"
+end
+
+# Debug Logging: Create a log-file
+if SHELL_DEBUG == true && !SHELL_LOG_FILE.nil?
+  File.open(SHELL_LOG_FILE, 'w') { |file| file.puts "Starting debug log..."}
+end
+
+##### Mail  #####
+MAIL_BCC = SHELL_CONFIG['mail']['bcc']
+
+# Access control
+ACCESS_RESTRICTED = SHELL_CONFIG['access_restricted']
+
+##### Time Slot for candles  #####
+PERIOD    = eval SHELL_CONFIG['period'] 
+TIME_SLOT = eval SHELL_CONFIG['time_slot']
